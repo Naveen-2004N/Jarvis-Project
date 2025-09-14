c7a57963a2066f9f1f17ec2ea8231bf3301b14f2
Naveen-2004N <navinmachari77@gmail.com>
2025-09-14 11:50:38 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os 
 import eel
+
+from engine.features import *
+
 eel.init("www")
+
+playAssistantSound()
+ 
 os.system('start msedge.exe --app="http://localhost:3000/www/assets/index.html"')
-eel.start("index.html",mode=None,host='localhost',block=True)
+eel.start("index.html",mode=None,host='localhost',block=True) 
--- www/assets/main.js
+++ www/assets/main.js
@@ -16,3 +16,34 @@
   toggle();
   setInterval(toggle, 2000); // loop
 })();
+
+// Siri Wave
+let siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style:"ios9",
+    speed:"0.15",
+    amplitude:"1",
+    autostart:true
+  });
+
+// Siri message animation
+(function animateMainText() {
+  const el = document.querySelector(".siri-message");
+  let showing = true;
+
+  function toggle() {
+    if (showing) {
+      el.classList.remove("fadeIn");
+      el.classList.add("animated", "fadeOutUp");
+    } else {
+      el.classList.remove("fadeOutUp");
+      el.classList.add("animated", "fadeIn");
+    }
+    showing = !showing;
+  }
+
+  toggle();
+  setInterval(toggle, 2000); // loop
+})();
